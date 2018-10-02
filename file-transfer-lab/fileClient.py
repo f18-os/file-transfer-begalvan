e3eed85456e9ac988456ea8e8ed28149d07e186d
begalvan <begalvan@miners.utep.edu>
2018-10-01 21:54:24 -0600

    final client file

---
+++ file-transfer-lab/fileClient.py
@@ -0,0 +1,36 @@
+#client file
+import socket
+ 
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect((socket.gethostname(), 50001))
+ 
+text_file = 'inputfile.txt'
+ 
+#Sending file
+with open(text_file, 'rb') as fs:
+    #Using with, no file close is necessary,
+    #with automatically handles file close
+    clientSocket.send(b'BEGIN')
+    while True:
+        data = fs.read(1024)
+        print('Sending data', data.decode('utf-8'))
+        clientSocket.send(data)
+        print('Sent data', data.decode('utf-8'))
+        if not data:
+            print('Breaking from sending data')
+            break
+    clientSocket.send(b'ENDED')
+    fs.close()
+ 
+#Receive file
+print("Receiving..")
+with open(text_file, 'wb') as fw:
+    while True:
+        data = clientSocket.recv(1024)
+        if not data:
+            break
+        fw.write(data)
+    fw.close()
+print("Received..")
+ 
+clientSocket.close()
