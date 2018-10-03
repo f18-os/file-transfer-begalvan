d3a1d967e97b8dddd055b7ab54c083decdaf9cc6
begalvan <begalvan@miners.utep.edu>
2018-10-03 00:03:10 -0600

    read all file

--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -32,3 +32,12 @@ while True:
         string = b"Append this to file."
         fa.write(string)
         fa.seek(0, 0)
+        print("Sending file.")
+        while True:
+            data = fa.read(1024)
+            conn.send(data)
+            if not data:
+                break
+        fa.close()
+        print("Sent file.")
+    break
