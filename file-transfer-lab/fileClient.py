ba42ba4672708c30b5caaeeebbd7c5d7f49cd0f6
begalvan <begalvan@miners.utep.edu>
2018-10-03 21:52:32 -0600

    rev not data

--- file-transfer-lab/fileClient.py
+++ file-transfer-lab/fileClient.py
@@ -13,11 +13,13 @@ if os.path.isfile(file_sent) and os.stat(file_sent).st_size !=0:
         clientSocket.send(b'START ')
         while True:
             data = fs.read(1024)
-            print('Transfering file', data.decode('utf-8')) #uses UTF-8 enconding
+            print("Transferring file", data.decode('utf-8')) #uses UTF-8 enconding
             clientSocket.send(data)
-            print('File sent', data.decode('utf-8'))
+            print("File sent", data.decode('utf-8'))
         if not data:
-            print('Breaking from transfering file')
+            print("Done with file")
+            done = False
+            print("Breaking from transfering file")
             break
     clientSocket.send(b'END ')
     fs.close()
