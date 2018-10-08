fb76016a670dd8a9d89f06158567df71ce736458
begalvan <begalvan@miners.utep.edu>
2018-10-07 22:02:50 -0600

    rev client

--- file-transfer-lab/fileClient.py
+++ file-transfer-lab/fileClient.py
@@ -8,31 +8,36 @@ clientSocket.connect((socket.gethostname(), 50001))
  
 file_sent = 'sentfile.txt'
 
+#testing file type and byte size
 if os.path.isfile(file_sent) and os.stat(file_sent).st_size !=0:  
+
     with open(file_sent, 'rb') as fs: #send textfile, handles file close
-        clientSocket.send(b'START ')
+        clientSocket.send(b'START')
         while True:
             data = fs.read(1024) 
             print("Transferring file", data.decode('utf-8')) #uses UTF-8 enconding
             clientSocket.send(data)
             print("File sent", data.decode('utf-8'))
+
             if not data:
-                print("Done with file")
+                print("No data on file")
                 done = False
                 print("Breaking from transfering file")
                 break
-    clientSocket.send(b'END ')
+    clientSocket.send(b'END')
     fs.close()
  
-#Receive file
-print("Receiving..")
-with open(file_sent, 'wb') as fw:
-    while True:
-        data = clientSocket.recv(1024)
-        if not data:
-            break
-        fw.write(data)
-    fw.close()
-print("Received")
+    #Receive file
+    #print("Receiving..")
+    with open(file_sent, 'wb') as fw:
+        while True:
+            data = clientSocket.recv(1024)
+            if not data:
+                break
+            fw.write(data)
+        fw.close()
+    print("Received")
  
-clientSocket.close()
+    clientSocket.close()
+else:
+    print("No file found")
