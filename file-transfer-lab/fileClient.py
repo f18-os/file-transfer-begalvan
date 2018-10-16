992228348e2a6db9010e53d44e3a9495e3f25c2b
begalvan <begalvan@miners.utep.edu>
2018-10-16 00:07:35 -0600

    rev send file

--- file-transfer-lab/fileClient.py
+++ file-transfer-lab/fileClient.py
@@ -3,29 +3,25 @@
 import socket
 import os
 
-clientsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsock.connect((socket.gethostname(), 50001))
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((socket.gethostname(), 50001))
 
 text_file = 'send_file.txt'
 
-if os.path.isfile(text_file) and os.stat(text_file).st_size != 0:
-    # Send file
-    with open(text_file, 'rb') as fileserver:
-        # Using with, no file close is necessary,
-        # with automatically handles file close
-        clientsock.send(b'start')
+def send_file():
+    with open(text_file, 'rb') as fs:
+        sock.send(b'start')
         while True:
-            data = fileserver.read(1024)
+            data = fs.read(1024)
             print('Sending data', data.decode('utf-8'))
-            clientsock.send(data)
+            sock.send(data)
             print('Sent data', data.decode('utf-8'))
             if not data:
                 print("File is empty now!")
-                done = False
                 print('Breaking from sending data')
                 break
-        clientsock.send(b'ENDED')
-        fileserver.close()
+        sock.send(b'end')
+        fs.close()
 
     # Receive file
     print("Receiving..")
