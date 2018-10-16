fb446b8237b5e65ed8d5bd0c2d41757484d769a9
begalvan <begalvan@miners.utep.edu>
2018-10-16 00:23:27 -0600

    rev handling file

--- file-transfer-lab/fileClient.py
+++ file-transfer-lab/fileClient.py
@@ -8,17 +8,17 @@ sock.connect((socket.gethostname(), 50001))
 
 text_file = 'send_file.txt'
 
-def send_file():
-    with open(text_file, 'rb') as fs:
+def send_file(): #send text file
+    with open(text_file, 'rb') as fs: #with handles file close
         sock.send(b'start')
         while True:
             data = fs.read(1024)
-            print('Sending data', data.decode('utf-8')) #uses utf-8 enconding
+            print("Sending data", data.decode('utf-8')) #uses utf-8 encoding
             sock.send(data)
-            print('Sent data', data.decode('utf-8'))
+            print("Sent data", data.decode('utf-8'))
             if not data:
-                print("File is empty now!")
-                print('Breaking from sending data')
+                print("File now empty")
+                print("Breaking from sending data")
                 break
         sock.send(b'end')
         fs.close()
@@ -37,8 +37,8 @@ def send_file():
     sock.close()
 
 def file_empty():
-    print("File does not exist or empty!")
-
+    print("No data on file found, or no file")
+#testing file type and byte size
 if os.path.isfile(text_file) and os.stat(text_file).st_size != 0:
    send_file()
 else:
